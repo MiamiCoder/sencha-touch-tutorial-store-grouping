040e8116a76ef42134f7a357f87cb0224b9a5bf2
MiamiCoder <miamicoder@gmail.com>
2014-06-04 15:21:44 -0400

    Finalized Sencha Touch Stores grouping example

---
+++ app.js
@@ -0,0 +1,63 @@
+﻿/* 
+Stores - client-side grouping.
+*/
+
+var loopsCounter = 0;
+
+Ext.application({
+    name: 'App',
+    models: ['Product'],
+    stores: ['Products'], 
+    launch: function () {
+
+        // Use the getStore function to get a reference to a store added via the stores config.
+        var productsStore = Ext.getStore('Products');
+
+        productsStore.on({
+            load: this.onStoreLoad
+        });
+
+        productsStore.load();
+
+        productsStore.setGrouper({
+            property: 'color',
+            direction: 'DESC'
+        });
+
+        // You have to reload for the new grouper to take effect.
+        productsStore.load();
+
+        productsStore.setGroupField('productLine');
+        productsStore.setGroupDir('DESC');
+        productsStore.load();
+
+        productsStore.setGrouper({
+            groupFn: function (record) {
+                return record.get('productName').substring(0,2);
+            },
+            sortProperty: 'productName'
+        });
+
+        productsStore.load();
+    },
+
+    onStoreLoad: function (store, records) {
+
+        var grouper = store.getGrouper();
+
+        if (loopsCounter < 3) {
+
+            console.log('Records grouped by ' + grouper.getProperty() + ' ' + grouper.getDirection() + ':');
+            store.each(function (record) {
+                console.log('- ' + record.get(grouper.getProperty()) + ' - ' + record.get('productName'));
+            });
+        } else {
+            console.log('Records grouped by first two chars of productName ' + grouper.getDirection() + ':');
+            store.each(function (record) {
+                console.log('- ' + record.get('productName'));
+            });
+        }
+
+        loopsCounter++;
+    }
+});      
---
+++ app/store/Products.js
@@ -0,0 +1,25 @@
+﻿Ext.define('App.store.Products', {
+    extend: 'Ext.data.Store',
+    config: {
+        model: 'App.model.Product',
+        remoteSort:false,
+        data: [
+            { productCode: 'S10_1678', productName: '1969 Harley Davidson Ultimate Chopper', productLine: 'Motorcycles', color:'Red' },
+            { productCode: 'S10_1949', productName: '1952 Alpine Renault 1300', productLine: 'Classic Cars', color: 'Silver' },
+            { productCode: 'S10_2016', productName: '1996 Moto Guzzi 1100i', productLine: 'Motorcycles', color: 'Silver' },
+            { productCode: 'S10_4698', productName: '2003 Harley-Davidson Eagle Drag Bike', productLine: 'Motorcycles', color: 'Silver' },
+            { productCode: 'S12_1666', productName: '1958 Setra Bus', productLine: 'Trucks and Buses', color: 'Green' },
+            { productCode: 'S10_4757', productName: '1972 Alfa Romeo GTA', productLine: 'Classic Cars', color: 'Red' },
+            { productCode: 'S12_4473', productName: '1957 Chevy Pickup', productLine: 'Trucks and Buses', color: 'Brown' },
+            { productCode: 'S18_1097', productName: '1940 Ford Pickup Truck', productLine: 'Trucks and Buses', color: 'Red' }
+        ],
+        grouper: {
+            property: 'productLine',
+            direction:'ASC'
+        },
+        sorters: [{
+            property: 'productCode',
+            direction: 'ASC'
+        }]
+    }
+});
